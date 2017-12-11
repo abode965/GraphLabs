09f07713b51ba125411426b85a15aab87c754bb6
abode965 <abode.himsy@gmail.com>
2017-12-11 10:39:14 +0200

    lab12

--- src/lab9/Match.java
+++ src/lab9/Match.java
@@ -1,6 +1,7 @@
 package lab9;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -27,8 +28,9 @@ public class Match {
 //        System.out.println(newc.isPerfect("0,1-2,3-5,6", g));
 //        System.out.println(newc.isAlternatingPath("0,6-2,3", "0,6-6,2-2,3-4,5", g));
          int[] a={0,2,3,4,6};
-        System.out.println(newc.isCovered(g,a ));
+      //  System.out.println(newc.isCovered(g,a ));
         System.out.println(newc.isIndependentSet(g, new int[]{1,4}));
+        System.out.println(newc.IsThorem(g, new int[]{1,4}));
 
     }
 
@@ -166,6 +168,7 @@ public class Match {
 
         return true;
     }
+    
     //lab 10 
     public boolean isCovered (UnweightedGraph g,int[] v){   
     List<Integer> tem=g.getVertices();
@@ -192,6 +195,32 @@ public class Match {
     }
     public boolean isKomus(UnweightedGraph g,int u , int v){
         return g.getNeighbors(u).contains(v);
+    }
+    
+    public boolean IsThorem(UnweightedGraph g , int []v){
+        if(!isIndependentSet(g, v))
+        return false ; 
+        int [] a = kesim(g.getVertices(), v);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        if(isCovered(g, kesim(g.getVertices(), v)))
+        return true ; 
+        
+        return false ;
+    }
+    
+    public int [] kesim (List<Object> a , int b[] ){
+       List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < b.length ; i++) {
+                a.remove((Integer)b[i]);
+                }
+            
+        int tempArray[]=new int[a.size()];
+        for (int i = 0; i < a.size(); i++) {
+            tempArray[i]=(int)a.get(i);
+        }
+        return tempArray ; 
     }
     
 }
